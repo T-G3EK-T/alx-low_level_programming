9d6bf666170f53d041eeb86a0723b1740a06ea3b
T-G3EK-T <oussamawahi123@gmail.com>
2023-03-16 05:09:13 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - 3aaaaawtani ila ma sm3touhach waaa katza3tar
+ * Return: 0 nbeg
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
