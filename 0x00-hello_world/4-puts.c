cb65f4f1586b1f92f3a51d428782c4d61ce1d016
T-G3EK-T <oussamawahi123@gmail.com>
2023-03-16 05:04:46 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main(void){
+/**
+*darouuuuuuri comment ta ntouma sa77a
+*bezzez wa hahowa
+*/
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
