7b31fed58ef52fe07415631e4d1d62ad3476d4d0
T-G3EK-T <oussamawahi123@gmail.com>
2023-03-17 04:48:47 -0700

    12. Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - wa ma kat3yawch men l7adia
+ * Return: zwa9 o lbokh
+ */
+int main(void)
+{
+int i, j;
+for (i = 0; i < 100; i++)
+{
+for (j = 0; j < 100; j++)
+{
+if (i < j)
+{
+putchar(48 + (i / 10));
+putchar(48 + (i % 10));
+putchar(' ');
+putchar(48 + (j / 10));
+putchar(48 +(j % 10));
+if (i + j != 197)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
