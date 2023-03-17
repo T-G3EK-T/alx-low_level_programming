8054b244009221f1bad9899511dba1dd316435ba
T-G3EK-T <oussamawahi123@gmail.com>
2023-03-17 04:08:41 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - wa katmouuuuuto
+*Return: 3la tb3kiiiiiik
+*/
+int main(void)
+{
+char c;
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
