2acfa3b257b7fb0768d87527f4558de2295d9039
T-G3EK-T <oussamawahi123@gmail.com>
2023-03-17 03:22:49 -0700

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - welah ma katmergo
+*Return: Dkhoul se77a hada
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+putchar('\n');
+return (0);
+}
