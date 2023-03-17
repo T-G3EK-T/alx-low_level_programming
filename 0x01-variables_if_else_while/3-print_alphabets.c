0a4d4b487cbd8608125bcb865e826a1059249329
T-G3EK-T <oussamawahi123@gmail.com>
2023-03-17 03:29:45 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - 3aya9to
+*Return: Lawah
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+for (c = 'A'; c <= 'Z'; c++)
+	putchar(c);
+putchar('\n');
+return (0);
+}
