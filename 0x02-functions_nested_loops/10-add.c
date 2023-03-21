b395021cc9eaf409d07c3db897bab5a181dabe12
T-G3EK-T <oussamawahi123@gmail.com>
2023-03-21 21:03:21 +0000

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isalpha - hiiiiippa
+ * Description: ba7bou7i
+ * @a: int
+ * @b: int
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+return(a + b);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -10,3 +8,4 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+int add(int, int);
