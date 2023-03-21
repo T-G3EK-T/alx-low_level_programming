2168e56756873c4e84d0c250f036abc967a92eb6
T-G3EK-T <oussamawahi123@gmail.com>
2023-03-21 15:36:31 +0000

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _islower - hiiiiippa
+ * Description: ba7bou7i
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+    return (1);
+return (0);
+}
