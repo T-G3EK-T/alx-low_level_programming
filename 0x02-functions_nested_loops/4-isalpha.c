3465b0586ee1d36ef6fce4d9aeb991512b4cd6b1
T-G3EK-T <oussamawahi123@gmail.com>
2023-03-21 16:19:24 +0000

    4. isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _islower - hiiiiippa
+ * _isalpha - hiiiiippa
  * Description: ba7bou7i
  * @c: Ascii code of a char
  * Return: Always 0.
  */
-int _islower(int c)
+int _isalpha(int c)
 {
 if (c >= 65 && c <= 122)
 {
