fcb82d9a54de48e540daaeae3315810434592f61
T-G3EK-T <oussamawahi123@gmail.com>
2023-03-21 06:56:50 -0700

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - fakhfakh
+ * Description: hanta chouf b3inik
+ * Return: siiiifer
+ */
+void print_alphabet(void)
+{
+char i;
+for (i = 'a' ; i <= 'z'; i++ )
+	_putchar(i);
+_putchar('\n');
+return (0);
+}
