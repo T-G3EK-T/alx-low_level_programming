570c5380cadb2e378e9897d3fcaf63e138ca4df6
T-G3EK-T <oussamawahi123@gmail.com>
2023-03-21 06:22:56 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+/**
+ * main tkharbi9a
+ * Return : lblasstha
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+	int i;
+
+	for (i = 0; i <= 7; i++)
+		putchar(str[i]);
+	putchar('\n');
+	return (0);
+}
