6affdb3509df2c62bca6b3750301a9d8c0554bb6
T-G3EK-T <oussamawahi123@gmail.com>
2023-03-21 17:11:12 +0000

    7. There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - hiiiiippa
+ * Description: ba7bou7i
+ * @c: Ascii code of a char
+ * Return: Always 0.
+ */
+int print_last_digit(int c)
+{
+c = c % 10
+if (c < 0)
+    c = -c;
+_putchar(48 + c);
+return (c);
+}
