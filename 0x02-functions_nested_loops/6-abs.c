3f3e73367958890b48d7ce1cb9ffb944c1df92e4
T-G3EK-T <oussamawahi123@gmail.com>
2023-03-21 16:41:18 +0000

    6. There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - hiiiiippa
+ * Description: ba7bou7i
+ * @x: Ascii code of a char
+ * Return: Always 0.
+ */
+int _abs(int x)
+{
+if (x < 0)
+{
+return (-x);
+}
+return (x);
+}
