de9b205ff349f688d835c2f99f8fe1c73e620da3
T-G3EK-T <oussamawahi123@gmail.com>
2023-03-21 18:38:18 +0000

    8. I'm federal agent Jack Bauer, and today is the longest day of my life

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * jack_bauer - hiiiiippa
+ * Description: ba7bou7i
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+int i,j,y,z,k;
+k = 5;
+for (i = 0; i <= 2; i++)
+{
+if (i == 2)
+{
+k = 3;
+}
+for (j = 0; j <= k; j++)
+{
+for (y = 0; y <= 5; y++)
+{
+for (z = 0; z <= 9; z++)
+{
+_putchar(48 + i);
+_putchar(48 + j);
+_putchar(':');
+_putchar(48 + y);
+_putchar(48 + z);
+_putchar('\n');
+}
+}
+}
+}
+}
