5cf4d88c74f51089d9440fc9eea05c4566c08cfc
T-G3EK-T <oussamawahi123@gmail.com>
2023-03-21 23:02:39 +0000

    13. Nature made the natural numbers; All else is the work of women

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - asfdsadfasdfasdfasdfasdf
+ * Description: asdljhaslfdjhsdakbcxvnxcmnv
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int s, n;
+for (n = 0; n < 1024; n++)
+{
+if (n % 3 == 0 || n % 5 == 0)
+{
+s += n;
+}
+}
+printf("%d\n", s);
+return (0);
+}
