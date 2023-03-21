19f99250c36a055d9d83c6fb7a4456dd7f4e3970
T-G3EK-T <oussamawahi123@gmail.com>
2023-03-21 16:26:26 +0000

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_sign - hiiiiippa
+ * Description: ba7bou7i
+ * @c: Ascii code of a char
+ * Return: Always 0.
+ */
+int print_sign(int n)
+{
+if ( n == 0)
+{
+_putchar('0');
+return (0);
+}
+else if ( n > 0)
+{
+_putchar('+');
+return (1);
+}
+_putchar('-');
+return (-1);
+}
