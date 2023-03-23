b61a85c3300ea1c0957a3e5ac571e4b994b5caeb
T-G3EK-T <oussamawahi123@gmail.com>
2023-03-23 17:02:26 +0000

    1. isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isdigit - do3a2
+ * Description: walaboda z3ma
+ * @c: Ascii code of a char
+ * Return: Always 0
+ */
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+return (0);
+}
