4a8c6fbbf9d9409d58ffe73408e68cf9d68ca3b7
T-G3EK-T <oussamawahi123@gmail.com>
2023-03-23 17:32:18 +0000

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - do3a2
+ * Description: walaboda z3ma
+ * @a: ra9em
+ * @b: ra9em
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+int i = 0;
+while (i < 10)
+{
+_putchar(48 + i);
+}
+_putchar('\n');
+}
