cef2bea48b19fb11f5ee35ebcb46531859d210a0
T-G3EK-T <oussamawahi123@gmail.com>
2023-03-23 12:07:51 +0000

    0. Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
-* main - wa b9a 7adini
-* Return: dakchi li 3tina
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
 */
 
 int main(void)
 {
-    int i;
+	int i;
 
-    i = 0;
-    positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-    return (0);
+	return (0);
 }
