fb3f4a94016d62da1c7bf82a2bb9507f4b6dc52a
T-G3EK-T <oussamawahi123@gmail.com>
2023-03-23 20:03:39 +0000

    7. I feel like I am diagonally parked in a parallel universe

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_diagonal - do3a2
+ * Description: walaboda z3ma
+ * @n: ra9em
+ * Return: Always 0
+ */
+void print_diagonal(int n)
+{
+int i, j;
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j < i; j++)
+{
+putchar(' ');
+}
+putchar(92);
+putchar('\n');
+}
+}
