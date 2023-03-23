88d25051762ff2c1c693c00f3a8f3613d4dd911a
T-G3EK-T <oussamawahi123@gmail.com>
2023-03-23 17:51:26 +0000

    4. I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - do3a2
+ * Description: walaboda z3ma
+ * Return: Always 0
+ */
+void print_most_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+if (i == 2 || i == 4)
+{
+continue;
+}
+putchar(48 + i);
+}
+putchar('\n');
+}
