d72c3bf075f5ead15cdc2af834bcb38f71a7a0e5
T-G3EK-T <oussamawahi123@gmail.com>
2023-04-05 23:37:25 +0000

    1-print_rev_recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - ma9louba
+ * @s: tahadi baghi t3rfha
+ * Return: khaltek
+*/
+
+void _print_rev_recursion(char *s)
+{
+    if (*s)
+            _print_rev_recursion(s + 1);
+            _putchar(*s);
+}
